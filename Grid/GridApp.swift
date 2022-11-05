fedc7de118457e289441ec27479f5901514ed204
Ayman <aymanjibrael@icloud.com>
2022-11-05 20:10:52 +0300

    Initial Commit

---
+++ Grid/GridApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridApp.swift
+//  Grid
+//
+//  Created by Ayhan on 5.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct GridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
